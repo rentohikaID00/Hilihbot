40ca91a96c9c626c66178d2e03dbcb48dbf337a9
rentohikaID00 <rentohika00@gmail.com>
2026-04-18 03:43:49 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,1 @@
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Halo! Bot aktif 👋")
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Ketik /start untuk memulai!")
-
-token = os.environ.get("TOKEN")
-app = ApplicationBuilder().token(token).build()
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("help", help_command))
-app.run_polling()
+token = "8544556604:AAEGfme74pm84ZrFJEhlHHk-DCnhndsEhf4"
